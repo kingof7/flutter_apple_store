a9899a48a52eac6f862dd1479c16a9e1cba21e93
vlfl0 <vlfl0@naver.com>
2023-12-10 21:48:32 +0900

    StatefulWidget 상태 관리 구현

--- lib/1-stateful_widget/cart.dart
+++ lib/1-stateful_widget/cart.dart
@@ -3,10 +3,17 @@ import 'package:app_store/common/product_tile.dart';
 import 'package:flutter/material.dart';
 
 class Cart extends StatelessWidget {
-  const Cart({super.key});
+  const Cart({
+    super.key,
+    required this.cartProductList,
+    required this.onPressed,
+  });
 
   /// 카트에 담긴 상품 목록
-  final List<Product> cartProductList = const [];
+  final List<Product> cartProductList;
+
+  /// 클릭 이벤트
+  final void Function(Product product) onPressed;
 
   @override
   Widget build(BuildContext context) {
@@ -28,8 +35,8 @@ class Cart extends StatelessWidget {
                 Product product = cartProductList[index];
                 return ProductTile(
                   product: product,
-                  isInCart: true,
-                  onPressed: (product) {},
+                  isInCart: true, // 부모로부터 받은 카트프로덕트리스트를 그대로 보여주기 때문에 true
+                  onPressed: onPressed, // 해당 상품을 눌렀을 때, 부모에게 전달
                 );
               },
             ),
--- lib/1-stateful_widget/store.dart
+++ lib/1-stateful_widget/store.dart
@@ -3,7 +3,17 @@ import 'package:app_store/common/product_tile.dart';
 import 'package:flutter/material.dart';
 
 class Store extends StatelessWidget {
-  const Store({super.key});
+  const Store({
+    super.key,
+    required this.cartProductList, // 부모(HomePage)로 부터 전달 받음
+    required this.onPressed, // 부모(HomePage)로 부터 전달 받음
+  });
+
+  /// 카트에 담긴 상품 목록
+  final List<Product> cartProductList;
+
+  /// 클릭 이벤트 함수 정의
+  final void Function(Product product) onPressed;
 
   @override
   Widget build(BuildContext context) {
@@ -14,8 +24,8 @@ class Store extends StatelessWidget {
           Product product = storeProductList[index];
           return ProductTile(
             product: product,
-            isInCart: false,
-            onPressed: (product) {},
+            isInCart: cartProductList.contains(product), // true or false
+            onPressed: onPressed, // 함수
           );
         },
       ),
