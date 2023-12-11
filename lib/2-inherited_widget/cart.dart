d41566199192a6123ea0e484fb91b4d9945998cc
jhlee <jhlee@sorin.co.kr>
2023-12-11 20:38:03 +0900

    InheritedWidget 실습 - updateShouldNotify

---
+++ lib/2-inherited_widget/cart.dart
@@ -0,0 +1,42 @@
+import 'package:app_store/2-inherited_widget/state/inherited_cart.dart';
+import 'package:app_store/common/product.dart';
+import 'package:app_store/common/product_tile.dart';
+import 'package:flutter/material.dart';
+
+class Cart extends StatelessWidget {
+  const Cart({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    // InheritedCart inheritedCart = InheritedCart.of(context);
+    InheritedCart inheritedCart = context.read();
+    final List<Product> cartProductList = inheritedCart.cartProductList;
+
+    return Scaffold(
+      body: cartProductList.isEmpty
+
+          /// Empty
+          ? const Center(
+              child: Text(
+                "Empty",
+                style: TextStyle(fontSize: 24, color: Colors.grey),
+              ),
+            )
+
+          /// Not Empty
+          : ListView.builder(
+              itemCount: cartProductList.length,
+              itemBuilder: (context, index) {
+                Product product = cartProductList[index];
+                return ProductTile(
+                  product: product,
+                  isInCart: true,
+                  onPressed: inheritedCart.onProductPressed,
+                );
+              },
+            ),
+    );
+  }
+}
---
+++ lib/2-inherited_widget/home_page.dart
@@ -0,0 +1,69 @@
+import 'package:app_store/2-inherited_widget/cart.dart';
+import 'package:app_store/2-inherited_widget/state/inherited_cart.dart';
+import 'package:app_store/2-inherited_widget/store.dart';
+import 'package:app_store/common/bottom_bar.dart';
+import 'package:app_store/common/product.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  /// 현재 선택된 index
+  int currentIndex = 0;
+
+  /// 카트에 담긴 상품 목록
+  List<Product> cartProductList = [];
+
+  /// 상품 클릭
+  void onProductPressed(Product product) {
+    setState(() {
+      if (cartProductList.contains(product)) {
+        // cartProductList.remove(product);
+        cartProductList = cartProductList.where((element) {
+          // 새로운 배열을 만들기에 참조가 달라짐
+          return element != product;
+        }).toList();
+      } else {
+        // cartProductList.add(product);
+        cartProductList = [...cartProductList, product]; // 새로 생성하기에 다른 참조를 가짐
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InheritedCart(
+      cartProductList: cartProductList,
+      onProductPressed: onProductPressed,
+      child: Scaffold(
+        body: IndexedStack(
+          index: currentIndex,
+          children: const [
+            /// Store
+            Store(),
+
+            /// Cart
+            Cart(),
+          ],
+        ),
+        bottomNavigationBar: Builder(
+          builder: (context) {
+            final inheritedCart = context.read<InheritedCart>();
+            return BottomBar(
+              currentIndex: currentIndex,
+              cartTotal: "${inheritedCart.cartProductList.length}",
+              onTap: (index) => setState(() {
+                currentIndex = index;
+              }),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
